11054e4a5243bec5d3316c3a3f759fe5adc10e0b
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-04 19:23:33 -0300

    Atulizará link para pt_br e unidade metrica

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const API_KEY = 'f25110b0f83adb9f7c080ee182cd1d00';
 
 let search= document.querySelector(".search");
 
-requestWeatherData(-13.851, -40.0812);
+requestWeatherData();
 
 
 function requestWeatherData(){
-    const promisse = axios.get(url);
-    promisse.then((request) => {console.log('Resposta', request)});
+    const promisse = axios.get(urlWeatherData(-13.851, -40.0812));
+    promisse.then((request) => {console.log('Resposta', request.data)});
     promisse.catch((request) => {console.log('Deu erro', request)});
 }
 
 function urlWeatherData(lat, lon){
-    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=pt_br`;
 }
 
 search.addEventListener("click",()=>{
